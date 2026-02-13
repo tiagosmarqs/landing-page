6c2fc90828ff9f09c08a7209f27b97175444ce0c
tiagosmarqs <tiagosmarques98@gmail.com>
2026-02-13 12:11:04 -0300

    first commit

---
+++ common/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>All in - Treinamentos</title>
+  <base href="http://localhost/">
+  <link rel="stylesheet" href="resources/css/main.css">
+</head>
+<body>
+  <header>
+    <div class="container">
+      <div class="conteudo">
+        <div class="logo">
+          <img src="resources/images/logo.png" alt="">
+        </div>
+        <div class="menu-lateral">
+          <ul class="nav">
+            <li>
+              <a href="#">Quem sou eu</a>
+            </li>
+            <li>
+              <a href="#">Eventos</a>
+            </li>
+            <li>
+              <a href="#">Cursos</a>
+            </li>
+            <li>
+              <a href="#">Videos</a>
+            </li>
+            <li>
+              <a href="#">Fale comigo</a>
+            </li>
+          </ul>
+          <div class="redes-sociais">
+            <p>Siga-me:</p>
+            <a href="#">Facebook</a>
+            <a href="#">Instagram</a>
+            <a href="#">Youtube</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+  require_once('common/header.php');
+  require_once('views/home.php');
+  require_once('common/footer.php');
+
+?>

e19c9ab3709b1a8b5b906d29976afd0d93d2d64f
tiagosmarqs <tiagosmarques98@gmail.com>
2026-02-26 13:02:30 -0300

    alterações aula 20

--- views/home.php
+++ views/home.php
@@ -1,2 +1,21 @@
 <main>
+  <section class="quem-sou-eu">
+    <div class="container">
+      <div class="img">
+        <img src="/resources/images/pessoa-banner.png" alt="">
+      </div>
+      <div class= "texto" >
+        <h2>Quem sou eu?</h2>
+        <div class="descricao">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod quas, saepe iusto voluptatem tempore, cupiditate ipsum, numquam impedit tenetur repellendus possimus voluptatum laborum beatae suscipit facere magni iste distinctio architecto?
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos veritatis veniam dolore atque dignissimos nulla iure rem porro sit commodi quod optio voluptas ratione deserunt officiis laboriosam beatae, at labore?
+          </p>
+          <a href="#" class="btn">Veja o video</a>
+        </div>
+      </div>
+    </div>
+  </section>
 </main>

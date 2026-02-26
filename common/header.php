806aa40c4e7507efba3c125fa4f2e7147c74a590
tiagosmarqs <tiagosmarques98@gmail.com>
2026-02-26 11:05:42 -0300

    alterações aula 19

--- common/header.php
+++ common/header.php
@@ -9,6 +9,8 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"/>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" ></script>
+  <script src="resources/js/script.js" ></script>
   <link rel="stylesheet" href="resources/css/main.css">
 </head>
 <body>
@@ -44,9 +46,7 @@
           </div>
         </div>
         <div class="nav-bar">
-          <i class= "fa-solid fa-bars-staggered">
-
-          </i>
+          <i class= "fa-solid fa-bars-staggered"></i>
         </div>
       </div>
     </div>

d9109f6c8a2d607c21124c8a56bb543002bfab32
tiagosmarqs <tiagosmarques98@gmail.com>
2026-03-13 13:20:58 -0300

    alterações aula 33

--- common/footer.php
+++ common/footer.php
@@ -1,2 +1,10 @@
+<footer>
+  <div class="container">
+    <div class="conteudo">
+      <p>Todos os direitos reservados</p>
+      <p>Desenvolvido por <a href="">Tiago</a><i class="fab fa-instagram"></i></p>
+    </div>
+  </div>
+</footer>
 </body>
 </html>
